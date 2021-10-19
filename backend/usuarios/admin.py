4900dd1243ff5abf9c7b33174472e22ed3183cc7
m.v.u <mrt.v.urisman@gmail.com>
2021-10-19 19:37:37 -0300

    Add model Cases

--- backend/usuarios/admin.py
+++ backend/usuarios/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from .models import User, Teacher, Client
+from .models import Case, User, Teacher, Client
 # Register your models here.
 
 admin.site.register(User)
 admin.site.register(Teacher)
 admin.site.register(Client)
+admin.site.register(Case)
--- backend/usuarios/models.py
+++ backend/usuarios/models.py
@@ -9,12 +9,32 @@ expertise_status = [
     ('F', 'Financiera')
 ]
 
+type_status = [
+    ('L', 'Legal'),
+    ('F', 'Financiera'),
+    ('A', 'Ambos')
+]
+
 careers_status = [
     ('abogado', 'Derecho'),
     ('ingeniero economico', 'Ingeniero en economia'),
     ('Ingeniero industrial', 'Ingeniero industrial'),
-    ('Ingeniero informatico', 'Ingeniero en informatica'),
+    ('Ingeniero informatico', 'Ingeniero en informatica')
 ]
+
+status = [
+    ('D', 'Disponible'),
+    ('A', 'Asignado'),
+    ('S', 'Solicitud Cierre'),
+    ('C', 'Cerrado')
+]
+
+chat_preference = [
+    ('0', 'ChatApp'),
+    ('1', 'Llamada'),
+    ('2', 'Ambas')
+]
+
 
 # Create your models here.
 
@@ -40,6 +60,9 @@ class Client(models.Model):
     ###     HEREDA DE LA TABLA USER     ###
     client= models.OneToOneField(
       User, on_delete=models.CASCADE, blank=True, null=True)
+    #cases = models.ManyToManyField(
+    #    Case, on_delete=models.CASCADE, blank=True, null=True)
+    
     ###     ATRIBUTOS     ###
     rut = models.CharField(max_length=12, verbose_name='RUT')
     phone = models.CharField(max_length=10, null=True, blank=True, verbose_name="Telefono")
@@ -60,6 +83,10 @@ class Teacher(models.Model):
     ###     HEREDA DE LA TABLA USER     ###
     teacher = models.OneToOneField(
         User, on_delete=models.CASCADE, blank=True, null=True)
+    #cases = models.ManyToManyField(
+    #    Case, on_delete=models.CASCADE, blank=True, null=True)
+
+
     ###     ATRIBUTOS     ###
     rut = models.CharField(max_length=12, verbose_name='RUT')
     phone = models.CharField(max_length=10, null=True, blank=True, verbose_name="Telefono")
@@ -88,4 +115,44 @@ class Teacher(models.Model):
         ordering = ('-created_at',)
     
     def __str__(self):
-      return self.teacher.username  
+      return self.teacher.username
+
+
+###     TABLA CASOS     ###
+class Case(models.Model):
+    ###     HEREDA DE LA TABLA USER     ###
+    case_teacher = models.OneToOneField(
+        Teacher, on_delete=models.CASCADE, blank=True, null=True)
+    case_client = models.OneToOneField(
+        Client, on_delete=models.CASCADE, blank=True, null=True)
+    
+    ###     ATRIBUTOS     ###
+    name = models.CharField(max_length=60)
+    files = models.FileField(upload_to='documents/cases/', verbose_name='Casos')
+    description = models.CharField(max_length=300)
+    type = models.CharField(
+        max_length=1,
+        null=False, blank=False,
+        choices=type_status,
+        default=""
+    )
+    status = models.CharField(
+        max_length=1,
+        null=False, blank=False,
+        choices=status,
+        default="Disponible"
+    )
+    chat_preference = models.CharField(
+        max_length=1,
+        null=False, blank=False,
+        choices=chat_preference,
+        default=""
+    )
+    
+    created_at = models.DateTimeField(auto_now_add=True,verbose_name="Fecha Creacion")
+    updated_at = models.DateTimeField(auto_now=True,verbose_name="Fecha Actualizacion")
+    finished_at = models.DateTimeField(auto_now=True,verbose_name="Fecha Cierre")
+
+    class Meta:
+        verbose_name = 'case'
+        verbose_name_plural = 'cases'
